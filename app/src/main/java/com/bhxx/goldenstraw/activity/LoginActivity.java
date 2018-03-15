82a65d1c0a35ccb78c953c662a59065edd00ed19
llxqb <llxqb_tt@126.com>
2018-03-15 11:25:05 +0800

    更新sdk

--- app/src/main/java/com/bhxx/goldenstraw/activity/LoginActivity.java
+++ app/src/main/java/com/bhxx/goldenstraw/activity/LoginActivity.java
@@ -1,7 +1,5 @@
 package com.bhxx.goldenstraw.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
